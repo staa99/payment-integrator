53ad45f9d6ef7e848351d111a503ab5c5d8544d7
staa99 <ahmadulfawwaz@gmail.com>
2019-01-05 01:33:31 +0100

    create bank transfer response stubs

---
+++ StaaPaymentIntegrator.Paystack/Implementations/Responses/BankTransfer/BankTransferRecipientCreationResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Staaworks.PaymentIntegrator.Paystack.Implementations.Responses.BankTransfer
+{
+    public class BankTransferRecipientCreationResponse
+    {
+    }
+}
